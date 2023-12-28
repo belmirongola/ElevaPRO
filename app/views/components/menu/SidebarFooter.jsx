f1f8779f2b968601e84ec8c965c223ac7f871160
Belmiro Ngola <belmirongola@gmail.com>
2023-12-28 03:03:04 +0100

    New commit

--- app/views/components/menu/SidebarFooter.jsx
+++ app/views/components/menu/SidebarFooter.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import { Typography } from '../default/Typography';
+import { Typography } from '../default/Typography.jsx';
 
 const StyledButton = styled.a`
   padding: 5px 16px;
